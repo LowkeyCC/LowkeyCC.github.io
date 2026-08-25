024108f2422cec66586c3d04e76309dfaea8eb4c
LowkeyCC <lowkeycc54@gmail.com>
2026-08-25 18:04:55 +0200

    Add wiki source: .vitepress/theme/Layout.vue

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <template v-if="isHome">
+    <HomeLayout />
+  </template>
+  <DefaultLayout v-else />
+  <AdminBar />
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useData } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+import HomeLayout from './HomeLayout.vue'
+import AdminBar from './AdminBar.vue'
+
+const { Layout: DefaultLayout } = DefaultTheme
+const { frontmatter } = useData()
+
+const isHome = computed(() => frontmatter.value?.layout === 'home')
+</script>

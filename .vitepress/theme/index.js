faa88ef8c00204ec84f80e809129eb895245cc42
LowkeyCC <lowkeycc54@gmail.com>
2026-08-26 12:59:18 +0200

    fix: masques listing via static JSON + remove dead API/admin

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,17 +1,12 @@
 import DefaultTheme from 'vitepress/theme'
 import Layout from './Layout.vue'
-import AdminBar from './AdminBar.vue'
+import MasqueList from './MasqueList.vue'
 import './custom.css'
-
-if (typeof window !== 'undefined') {
-  const isVite = (location.port === '5173' || location.port === '5174')
-  window.__WIKI__ = { id: 'carnival-wiki', api: isVite ? 'http://localhost:8787' : '' }
-}
 
 export default {
   extends: DefaultTheme,
   Layout,
   enhanceApp({ app }) {
-    app.component('AdminBar', AdminBar)
+    app.component('MasqueList', MasqueList)
   }
 }
